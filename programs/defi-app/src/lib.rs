24cc4fa840034db18969dccd541e4f2dd81e4758
btdev0306 <btdev0306@outlook.live.com>
2024-06-04 08:16:45 -0500

    update

--- programs/defi-app/src/lib.rs
+++ programs/defi-app/src/lib.rs
@@ -6,10 +6,35 @@ declare_id!("6w1nyi94G8Q65gWinzVXEj38AwnCMqzsGFFpzB4Y6zNY");
 pub mod defi_app {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(ctx: Context<Create>) -> Result<()> {
+        let base_account=&mut ctx.accounts.base_account;
+        base_account.count=0;
+        Ok(())
+    }
+
+    pub fn increment(ctx:Context<Increment>)->Result<()>{
+        let base_account=&mut ctx.accounts.base_account;
+        base_account.count+=1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer=user, space=16+16)]
+    pub base_account:Account<'info,BaseAccount>,
+    #[account(mut)]
+    pub user:Signer<'info>,
+    pub system_program:Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info>{
+    #[account(mut)]
+    pub base_account:Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount{
+    pub count: u64,
+}
--- tests/defi-app.js
+++ tests/defi-app.js
@@ -1,13 +1,37 @@
 const anchor = require("@coral-xyz/anchor");
-
+const assert=require('assert');
+const {SystemProgram}=anchor.web3;
 describe("defi-app", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider=anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.DefiApp;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.DefiApp;
-    const tx = await program.methods.initialize().rpc();
+  it("Creates a counter", async () => {
+    const baseAccount=anchor.web3.Keypair.generate();
+    const tx=await program.rpc.create({
+      accounts:{
+        baseAccount:baseAccount.publicKey,
+        user:provider.wallet.publicKey,
+        systemProgram:SystemProgram.programId,
+      },
+      signers:[baseAccount]
+    });
+    const account=await program.account.baseAccount.fetch(baseAccount.publicKey);
     console.log("Your transaction signature", tx);
+    console.log('count 0:', account.count.toString());
+    assert.ok(account.count.toString()==0);
+    _baseAccount=baseAccount;
+  });
+  it("Increments the counter",async()=>{
+    const baseAccount=_baseAccount;
+    await program.rpc.increment({
+      accounts:{
+        baseAccount:baseAccount.publicKey,
+      },
+    });
+    const account=await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Count 1: ', account.count.toString());
+    assert.ok(account.count.toString()==1);
   });
 });
